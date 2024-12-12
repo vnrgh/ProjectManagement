0f426d6aece79747bad557f156d3fc74d1ae0e55
Daniil <dandrabskiy@gmail.com>
2024-12-12 12:01:19 +0300

    small fixes

--- mail/src/main/java/example/spring/service/CustomMailSender.java
+++ mail/src/main/java/example/spring/service/CustomMailSender.java
@@ -4,6 +4,7 @@ package example.spring.service;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.MailException;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
@@ -22,7 +23,6 @@ public class CustomMailSender {
         this.mailSender = mailSender;
     }
 
-    //todo поигарться с настройками sendMail
     public void sendMail(String to, String data) {
         try {
             logger.info("Mail preparation started");
@@ -32,8 +32,9 @@ public class CustomMailSender {
             simpleMailMessage.setTo(to);
             simpleMailMessage.setText(data);
             mailSender.send(simpleMailMessage);
-        } catch (Exception e) {
-            throw new RuntimeException(e.getMessage());
+        } catch (MailException e) {
+            logger.error(e.getMessage());
+//            throw new Runtime(e.getMessage());
         }
     }
 }
--- rest_api/src/main/java/example/spring/kafka/config/KafkaTopicConfig.java
+++ rest_api/src/main/java/example/spring/kafka/config/KafkaTopicConfig.java
@@ -1,6 +1,8 @@
 package example.spring.kafka.config;
 
 import org.apache.kafka.clients.admin.NewTopic;
+import org.apache.kafka.common.protocol.types.Field;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -10,17 +12,23 @@ import org.springframework.kafka.config.TopicBuilder;
 @Profile("!test")
 public class KafkaTopicConfig {
 
+    @Value("${spring.kafka.topic.welcomeMailTopic}")
+    private String welcomeMailTopic;
+
+    @Value("${spring.kafka.topic.testTopic}")
+    private String testTopic;
+
     @Bean
     public NewTopic testTopic() {
         return TopicBuilder
-                .name("testTopic")
+                .name(testTopic)
                 .build();
     }
 
     @Bean
     public NewTopic welcomeMailTopic() {
         return TopicBuilder
-                .name("welcomeMailTopic")
+                .name(welcomeMailTopic)
                 .build();
     }
 }
